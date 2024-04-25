83c27a897c8fb9c37ba879e0673f6d88e134623d
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-04-25 11:28:43 -0500

    CORS

---
+++ src/main/java/med/voll/api/infra/security/CorsConfig.java
@@ -0,0 +1,28 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig  {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("https://medical-app-gpi.web.app"); // Permitir acceso desde esta URL
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Agrega las cabeceras que necesitas permitir
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        System.out.println("Se habilita configuracion de CORS " + source.toString());
+        return source;
+    }
+}
